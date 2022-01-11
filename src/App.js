3b0360566556a14c25275ef0354d47123dca9595
damisa25 <damisa.25@mail.kmutt.ac.th>
2022-01-12 03:43:44 +0700

    v.1

---
+++ src/App.js
@@ -0,0 +1,161 @@
+import { Col, Row,Breadcrumb,DatePicker, Space, Table, Spin,Layout } from 'antd';
+import axios from "axios";
+import { HomeFilled } from '@ant-design/icons';
+import './index.scss';
+import NavBar from './components/navBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee, faChartBar, faDownload, faPrint, faChevronDown, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
+
+library.add(fab, faCheckSquare, faCoffee)
+
+const api = "https://wegivmerchantapp.firebaseapp.com/exam/bi-member-day-2020-04-01.json"
+const getDatas = async () => {
+  return await axios.get(api);
+}
+
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'customername',
+    key: 'customername',
+    
+  },
+  {
+    title: 'ID',
+    dataIndex: 'customerphone',
+    key: 'customerphone',
+   
+  },
+  {
+    title: 'Tier',
+    dataIndex: 'customertier',
+    key: 'customertier',
+    width: 100,
+  },
+  {
+    title: 'LTV',
+    dataIndex: 'totalamount',
+    key: 'totalamount',
+  },
+  {
+    title: 'Total Trans',
+    dataIndex: 'totaltransaction',
+    key: 'totaltransaction',
+  },
+  {
+    title: 'Total Point',
+    dataIndex: 'totalreward',
+    key: 'totalreward',
+  },
+  {
+    title: 'Remaining Point',
+    dataIndex: 'remainingpoint',
+    key: 'remainingpoint',
+  },
+];
+
+const App = () => {
+  const {Footer} = Layout;
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState({})
+  const yearFormat = 'YYYY';
+
+  const allData = async () => {
+    try {
+      const res = await getDatas();
+      setData(res.data);
+      if (res) {
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    allData();
+  }, []);
+
+  return (
+    <>
+    <NavBar />
+    <div className='container'>
+    <Spin spinning={loading} tip="Loading...">
+      <Row justify='space-between' align='middle'>
+        <Col span={12}>
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            <HomeFilled />
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Business Insight</Breadcrumb.Item>
+          <Breadcrumb.Item>Report</Breadcrumb.Item>
+          <Breadcrumb.Item>Member</Breadcrumb.Item>
+          <Breadcrumb.Item>Member</Breadcrumb.Item>
+        </Breadcrumb>
+        </Col>
+        <Col span={12} className='align-end'>
+          <div className='circle-box'><FontAwesomeIcon icon={faChartBar} /></div>
+          <div className='circle-box'><FontAwesomeIcon icon={faDownload} /></div>
+          <div className='circle-box'><FontAwesomeIcon icon={faPrint} /></div>
+        </Col>
+      </Row>
+      <Row justify='space-between' align='middle' className='mt-1'>
+        <Col span={12} className='text title'>Yearly Member <span className='font-normal'>01-Jan-2020 to 31-Dec-2020</span></Col>
+        <Col span={12} className='align-end'>
+          <DatePicker className='year-picker' placeholder='Year View' format={yearFormat} picker="year" suffixIcon={<><FontAwesomeIcon icon={faChevronDown} /></>} allowClear/>
+          <div className='rec-box'>2020</div>
+          <div className='rec-box i-calendar'><FontAwesomeIcon icon={faCalendar} /></div>
+        </Col>
+      </Row>
+      <Row className='sum-tier'>
+      
+        <Col span={8} className='sum-tier orange'>
+        <Space direction="vertical" style={{width: '100%', gap: 30}}>
+          <Row justify='space-between' >
+            <Col>Total <span className='text big'>Members</span> :</Col>
+            <Col className='text big'>5</Col>
+          </Row>
+          <Row justify='space-between' >
+            <Col>Total <span className='text big'>Rev.</span><span className='text small'>(THB)</span> :</Col>
+            <Col className='text big'>639K</Col>
+          </Row>
+        </Space>
+        </Col>
+        <Col span={16} className='sum-tier gray'>
+          <Row justify='center'><Col span={24} className='text big text-center'>tttt</Col></Row>
+          <Row justify='space-between'>
+            <Col>Total <span className='text big'>Members</span> :</Col>
+            <Col className='text big'>5</Col>
+          </Row>
+          <Row justify='space-between'>
+            <Col>Total <span className='text big'>Rev.</span><span className='text small'>(THB)</span> :</Col>
+            <Col className='text big'>639K</Col>
+          </Row>
+        </Col>
+      </Row>
+      <Table 
+        rowClassName={(record, index) => index % 2 === 0 ? 'table-row-white' :  'table-row-gray'}
+        columns={columns} 
+        dataSource={data.list} 
+        bordered  
+        scroll={{ y: 300 }} />
+      <div className='sum-all'> 
+      <Row>
+        <Col span={8}>Total</Col>
+        <Col span={4} className='align-end' >639498</Col>
+        <Col span={4} className='align-end'>7</Col>
+        <Col span={4} className='align-end'>47699</Col>
+        <Col span={4} className='align-end'>47699</Col>
+      </Row>
+      </div>
+      </Spin>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,34 @@
+import { Col, Row,Avatar } from 'antd';
+import { UserOutlined,HeartFilled } from '@ant-design/icons';
+import logo from '../logo.png'
+import './navBar.scss';
+
+const NavBar = () => {
+  return (
+    <div className='navbar-container'>
+    <Row className='navbar'  >
+      <Col span={18} className='black'>
+        <Row align='middle' justify='space-between'>
+          <Col span={20}>
+            <img className='logo' src={logo}/>
+          </Col>
+          <Col span={4} >
+            <Row align='middle'  >
+              <Col>
+                <Avatar icon={<UserOutlined size="small" className='i-user'/>} />
+              </Col>
+              <Col className='text-user'>
+                Mr.Admin
+                <span className='pos'>Head Quarter</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={6} className='orange'>MENU <HeartFilled /></Col>
+    </Row>
+    </div>
+  );
+}
+
+export default NavBar;
